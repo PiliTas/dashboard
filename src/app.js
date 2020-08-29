8b4500274468f751365eeb2f38767d8fdc6881bc
NPGonzalez <naypili@gmail.com>
2020-08-29 15:18:30 -0400

    Se agrega títulos a la página del color del botón asociado

--- src/app.js
+++ src/app.js
@@ -12,6 +12,9 @@ document.getElementById('employees').addEventListener('click', function() {
         })
         .then(data => {
             const element = data.results;
+            document.getElementById('subtitle').style.color = 'rgb(37, 134, 163)';
+            document.getElementById('subtitle').innerHTML = `<h1>Empleados</h1>`;
+
             console.log(element);
         })
         .catch((error) => {
