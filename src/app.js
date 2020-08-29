01e7b9e636b83ef49a4d52a11781718ef266998f
NPGonzalez <naypili@gmail.com>
2020-08-29 15:34:59 -0400

    Se crea template de tajeta para información de contacto de empleado

--- src/app.js
+++ src/app.js
@@ -15,7 +15,22 @@ document.getElementById('employees').addEventListener('click', function() {
             document.getElementById('subtitle').style.color = 'rgb(37, 134, 163)';
             document.getElementById('subtitle').innerHTML = `<h1>Empleados</h1>`;
 
-            console.log(element);
+            document.getElementById('datacontainer').innerHTML = '';
+            for (let i = 0; i < element.length; i += 1) {
+                document.getElementById('datacontainer').innerHTML +=
+                    `<div class="cards"> 
+                        <ul>
+                            <li><div class ="card"><img src= ${element[i].picture.large}>
+                            <ul>
+                                <li>${element[i].name.title} ${element[i].name.first} ${element[i].name.last}</li>
+                                <li>Cel.: ${element[i].cell}</li>
+                                <li>Dirección: ${element[i].location.street.name} ${element[i].location.street.number}</li>
+                                <li>${element[i].location.postcode} - ${element[i].location.country}</li>
+                            </ul>
+                                 </div>
+                        </ul>
+                    </div> `;
+            }
         })
         .catch((error) => {
             console.log('error', error)
