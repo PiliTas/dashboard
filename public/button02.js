ad8834b85736a47c5f329ab5487a1624baaa7bab
NPGonzalez <naypili@gmail.com>
2020-08-31 18:40:22 -0400

    Se establece llamado a función perfiles

--- public/button02.js
+++ public/button02.js
@@ -1,44 +1,37 @@
 export default function dataProfiles() {
-    // FUNCIONALIDAD BOTÓN CATEGORÍAS DE ACUERDO A LOS AÑOS DE SERVICIO (REGISTRO)
-    // JUNIOR  0 < AÑOS DE SERVICIO < 5
-    // SEMI-SENIOR 5 <= AÑOS DE SERVICIOS < 10
-    // SENIOR aÑOS DE SERVICIOS >= 10
 
+    // FUNCIONALIDAD BOTÓN PERFILES
+    // ESTA FUNCIONALIDAD PERMITE OBTENER EL PERFIL DE USUARIO (DDATOS DEMOGRÁFICOS)
 
     fetch('https://randomuser.me/api/?results=20')
         .then((response) => {
             if (response.ok) {
-                let data = response.json()
+                let data = response.json();
                 return data;
             }
             throw response;
         })
         .then(data => {
             const element = data.results;
-            let category = "";
-            document.getElementById('subtitle').style.color = '#1d2731';
-            document.getElementById('subtitle').innerHTML = `<h1>Categorías</h1>`;
+            let genders = "";
+            document.getElementById('subtitle').style.color = '#d98310';
+            document.getElementById('subtitle').innerHTML = `<h1>Perfiles</h1>`;
             document.getElementById('datacontainer').innerHTML = '';
             for (let i = 0; i < element.length; i += 1) {
-                if (element[i].registered.age > 0 && element[i].registered.age < 5) {
-                    category = "Junior";
+                if (element[i].gender === "female") {
+                    genders = "mujer";
                 } else {
-                    if (element[i].registered.age >= 5 && element[i].registered.age < 10) {
-                        category = "Semi-senior";
-                    } else {
-                        if (element[i].registered.age >= 10) {
-                            category = "Senior";
-                        }
-                    }
+                    genders = "hombre"
                 }
                 document.getElementById('datacontainer').innerHTML +=
                     `<div class="cards"> 
                         <ul>
                             <li><div class ="card"><img src= ${element[i].picture.large}>
                             <ul>
-                                <li>Nombre: ${element[i].name.first} ${element[i].name.last}</li>
-                                <li>Años de servicio: ${element[i].registered.age}</li>
-                                <li>Categoría: ${category}</li>
+                                <li>Nombre: ${element[i].name.title} ${element[i].name.first} ${element[i].name.last}</li>
+                                <li>Género: ${genders}</li>
+                                <li>Edad: ${element[i].dob.age}</li>
+                                <li>Nacionalidad: ${element[i].nat}</li>
                             </ul>
                                 </div>
                         </ul>
@@ -48,6 +41,8 @@ export default function dataProfiles() {
         .catch((error) => {
             console.log('error', error)
         })
+
+
 
 
 }
--- public/main.js
+++ public/main.js
@@ -1,8 +1,12 @@
-import dataEmployees from './datas.js'
-
+import dataEmployees from './button01.js';
+import dataProfiles from './button02.js';
 const showEmployees = () => {
-
     return dataEmployees();
 }
 
+const showProfiles = () => {
+    return dataProfiles();
+}
+
 document.getElementById('employees').addEventListener('click', showEmployees);
+document.getElementById('profiles').addEventListener('click', showProfiles);
